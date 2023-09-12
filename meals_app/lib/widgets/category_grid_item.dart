307835f1b199984ce46161085a79b82a3f3b56fe
DungxLas <85223326+DungxLas@users.noreply.github.com>
2023-09-12 10:46:32 +0700

    12/09/2023

---
+++ meals_app/lib/widgets/category_grid_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meals.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  final Category category;
+  final VoidCallback onSelectCategory;
+
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<Meal> listMeals = dummyMeals
+        .where((element) => element.categories.contains(category.id))
+        .toList();
+
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          border: Border.all(width: 2),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
